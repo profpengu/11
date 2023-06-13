efdb3bcf84f2cb8c8a64ae1b0b747764803e1e31
Yashas Karre <yashas.karre@gmail.com>
2023-06-13 15:08:13 -0400

    balloon

--- main.c
+++ main.c
@@ -8,19 +8,21 @@ struct Balloon {
 
 int main()
 {
-   const int screenWidth = 800;
-   const int screenHeight = 450;
+   const int screenWidth = 1280;
+   const int screenHeight = 720;
    InitWindow( screenWidth, screenHeight, "tower defense" );
    const Vector2 ballonSpawnPoint = { (float)screenWidth/2, (float)screenHeight/2};
    const Texture2D backgroundTexture = LoadTexture("./background.png");
+   const Texture2D balloonimage = LoadTexture("./balloon.png");
    SetTargetFPS(60);
    while (!WindowShouldClose())
    {
     BeginDrawing();  
         ClearBackground(RAYWHITE);
-        DrawTexture(backgroundTexture, 0, 0, WHITE);
-    EndDrawing();
-
+        // DrawTexturePro(backgroundTexture, (Rectangle){0, 0, backgroundTexture.width, backgroundTexture.height}, (Rectangle){0, 0, screenWidth, screenHeight}, (Vector2){0, 0}, 0.f, WHITE);
+        DrawTexture(balloonimage, 20, 20, RED);
+       EndDrawing();
+  
     }
     
     CloseWindow();
